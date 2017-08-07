d490c5da5ac97f1f54afcecf9406fcb5943ff979
jayrbarr <jayrbarr@gmail.com>
2017-08-07 09:11:47 -0400

    Added comments, docstrings. Fixed PEP8 errors.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,31 +1,56 @@
 import media
 import fresh_tomatoes
 
-tommy_boy = media.Movie("Tommy Boy",
-                        "A college dropout has to save his father's company.",
-                        "https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Tommy_Boy.jpg/220px-Tommy_Boy.jpg",
-                        "https://www.youtube.com/watch?v=6nQ4K2bvVxY&t=13s")
+# Tommy Boy Movie - title, storyline, poster, trailer
+tommy_boy = media.Movie(
+    "Tommy Boy",
+    "A college dropout has to save his father's company.",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/f/f8/Tommy_Boy.jpg/220px-Tommy_Boy.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=6nQ4K2bvVxY&t=13s")
 
-the_incredibles = media.Movie("The Incredibles", "Superhero family comes out of retirement.", "https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/The_Incredibles.jpg/220px-The_Incredibles.jpg",
-                     "https://www.youtube.com/watch?v=eZbzbC9285I")
+# Tommy Boy Movie - title, storyline, poster, trailer
+the_incredibles = media.Movie(
+    "The Incredibles",
+    "Superhero family comes out of retirement.",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/The_Incredibles.jpg/220px-The_Incredibles.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=eZbzbC9285I")
 
-happy_gilmore = media.Movie("Happy Gilmore", "Washout hockey player becomes golf sensation.",
-                             "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Happygilmoreposter.jpg/220px-Happygilmoreposter.jpg",
-                             "https://www.youtube.com/watch?v=y1emDAYCfVQ")
+# Tommy Boy Movie - title, storyline, poster, trailer
+happy_gilmore = media.Movie(
+    "Happy Gilmore",
+    "Washout hockey player becomes golf sensation.",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Happygilmoreposter.jpg/220px-Happygilmoreposter.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=y1emDAYCfVQ")
 
-indiana_jones = media.Movie("Raiders of the Lost Ark", "Archaeology professor recovers Ark of the Covenant.",
-                          "https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Raiders_of_the_Lost_Ark.jpg/220px-Raiders_of_the_Lost_Ark.jpg",
-                          "https://www.youtube.com/watch?v=XkkzKHCx154")
+# Tommy Boy Movie - title, storyline, poster, trailer
+indiana_jones = media.Movie(
+    "Raiders of the Lost Ark",
+    "Archaeology professor recovers Ark of the Covenant.",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Raiders_of_the_Lost_Ark.jpg/220px-Raiders_of_the_Lost_Ark.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=XkkzKHCx154")
 
-pirates_caribbean = media.Movie("Pirates of the Caribbean", "Cursed undead pirates wreak havoc until another pirate intervenes.",
-                                "https://upload.wikimedia.org/wikipedia/en/8/89/Pirates_of_the_Caribbean_-_The_Curse_of_the_Black_Pearl.png",
-                                "https://www.youtube.com/watch?v=naQr0uTrH_s")
+# Tommy Boy Movie - title, storyline, poster, trailer
+pirates_caribbean = media.Movie(
+    "Pirates of the Caribbean",
+    "Cursed undead pirates wreak havoc until another pirate intervenes.",
+    "https://upload.wikimedia.org/wikipedia/en/8/89/Pirates_of_the_Caribbean_-_The_Curse_of_the_Black_Pearl.png",  # NOQA
+    "https://www.youtube.com/watch?v=naQr0uTrH_s")
 
-matrix = media.Movie("The Matrix", "We are all really just batteries.",
-                           "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
-                           "https://www.youtube.com/watch?v=vKQi3bBA1y8")
+# Tommy Boy Movie - title, storyline, poster, trailer
+matrix = media.Movie(
+    "The Matrix",
+    "We are all really just batteries.",
+    "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",  # NOQA
+    "https://www.youtube.com/watch?v=vKQi3bBA1y8")
 
-movies = [tommy_boy, the_incredibles, happy_gilmore, indiana_jones, pirates_caribbean, matrix]
+# list of Movies used to generate media file
+movies = [
+    tommy_boy,
+    the_incredibles,
+    happy_gilmore,
+    indiana_jones,
+    pirates_caribbean,
+    matrix]
+
+# generate webpage and open in browser via fresh_tomatoes.py
 fresh_tomatoes.open_movies_page(movies)
-
-                          
--- media.py
+++ media.py
@@ -1,7 +1,20 @@
+""" this class defines a Movie type to hold an individual Movie """
+
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, movie_poster, movie_trailer):
+
+    """    Movie constructor with args:
+            movie_title = title string,
+            movie_storyline = storyline string,
+            movie_poster = poster url, and
+            movie_trailer = trailer url
+    """
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 movie_poster,
+                 movie_trailer):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_poster
         self.trailer_youtube_url = movie_trailer
-
